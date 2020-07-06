2db2e4815bceba43deb719210818b7e36c5bfcde
Arc <33088785+arcbtc@users.noreply.github.com>
2020-07-06 20:13:19 +0100

    Update Loop.ino

--- Loop.ino
+++ Loop.ino
@@ -11,8 +11,10 @@ void loop() {
   delay(5);
   // FETCH NODE DETAILS EVERY 60SECS
   if (now - then > 60000){ 
+    lv_obj_clean(lv_scr_act());
     get_display_info();
     then = millis();
     Serial.println("1 min passed");
   }
+
 }
